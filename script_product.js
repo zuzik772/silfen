fc85331decddd2d3301d0829dcf44cad7f3c1637
Zuzana Chudinova <zuzka.chudinova@gmail.com>
2021-11-16 21:35:51 +0100

    fetch product page

--- script_product.js
+++ script_product.js
@@ -1,24 +1,25 @@
 const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("id");
 
-const url = "https://zuzanacreates.com/wp21d/wp-json/wp/v2/bag?_embed";
+const url = "https://zuzanacreates.com/wp21d/wp-json/wp/v2/bag/" + id;
 
-getData();
-function getData() {
-  fetch(url)
-    .then(function (response) {
-      return response.json();
-    })
-    .then((data) => data.forEach(showOneItem));
-}
+fetch(url)
+  .then(function (response) {
+    return response.json();
+  })
+  // .then((data) => data.forEach(showOneItem));
+  .then(showOneItem);
 
 function showOneItem(item) {
   console.log(item);
   console.log("it works");
-  console.log(item._links["wp:attachment"][0].href);
+
+  //
+  //
+  // console.log(item._links["wp:attachment"][0].href);
 
   //   myClone.querySelector(".title").textContent = item.bag_name;
-  //   myClone.querySelector(".price").textContent = item.price;
+  // myClone.querySelector(".price").textContent = item.price;
   const urlGallery =
     "https://zuzanacreates.com/wp21d/wp-json/wp/v2/media?parent=" + id;
   fetch(urlGallery)
@@ -30,10 +31,29 @@ function showOneItem(item) {
   function showProduct(imageData) {
     console.log("hi");
     console.log(imageData);
-    const myTemplate = document.querySelector("template").content;
+    const myTemplate = document.querySelector(".imageTemplate").content;
     const myClone = myTemplate.cloneNode(true);
     myClone.querySelector("img").src = imageData.source_url;
 
-    document.querySelector(".products").appendChild(myClone);
+    document.querySelector(".product_page article").appendChild(myClone);
   }
+
+  const template = document.querySelector(".productInfoTemplate").content;
+  const clone = template.cloneNode(true);
+  clone.querySelector(".nameBag").textContent = item.bag_name;
+  console.log(item.bag_name);
+  clone.querySelector(".price").textContent = item.price;
+  clone.querySelector(".description").textContent = item.description;
+  clone.querySelector(".material").textContent = item.material;
+  clone.querySelector(".dimensions").textContent = item.dimensions;
+
+  document.querySelector(".product_page article").appendChild(clone);
 }
+// showDetails();
+// function showDetails() {
+//   const template = document.querySelector("template").content;
+//   const clone = template.cloneNode(true);
+//   clone.querySelector(".price").textContent = item.price;
+
+//   document.querySelector(".products").appendChild(clone);
+// }
