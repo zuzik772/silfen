0d736e3e141d62d1105e6046b1d8bab97c6ee502
Zuzana Chudinova <zuzka.chudinova@gmail.com>
2021-11-17 12:32:56 +0100

    updated layout

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 const url = "https://zuzanacreates.com/wp21d/wp-json/wp/v2/bag?_embed";
 
-getData();
-function getData() {
-  fetch(url)
-    .then(function (response) {
-      return response.json();
-    })
-    .then(showItems);
-}
+fetch(url)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(showItems);
 
 function showItems(items) {
   console.log(items);
@@ -22,14 +19,6 @@ function showOneItem(item) {
   myClone.querySelector(".price").textContent = `${item.price} DKK`;
   myClone.querySelector("a").setAttribute("href", "product.html?id=" + item.id);
   myClone.querySelector("img").src =
-    item._embedded[
-      "wp:featuredmedia"
-    ][0].media_details.sizes.medium_large.source_url;
-
-  //   if (item.instock == 1) {
-  //     myClone.querySelector("img").classList.add("soldout");
-  //     myClone.querySelector("aside").textContent = item.title.rendered;
-  //     console.log(item.instock);
-  //   }
+    item._embedded["wp:featuredmedia"][0].media_details.sizes.medium.source_url;
   document.querySelector(".products").appendChild(myClone);
 }
--- script_product.js
+++ script_product.js
@@ -7,19 +7,15 @@ fetch(url)
   .then(function (response) {
     return response.json();
   })
-  // .then((data) => data.forEach(showOneItem));
   .then(showOneItem);
 
 function showOneItem(item) {
   console.log(item);
   console.log("it works");
 
-  //
-  //
-  // console.log(item._links["wp:attachment"][0].href);
-
-  //   myClone.querySelector(".title").textContent = item.bag_name;
-  // myClone.querySelector(".price").textContent = item.price;
+// get image gallery of one bag on the product page 
+// grab the parent element in this case one of pods called bag in WP 
+// to upload several images we used a post that has gallery of images in WP and found the route in json 
   const urlGallery =
     "https://zuzanacreates.com/wp21d/wp-json/wp/v2/media?parent=" + id;
   fetch(urlGallery)
@@ -27,18 +23,17 @@ function showOneItem(item) {
       return response.json();
     })
     .then((data) => data.forEach(showProduct));
-
+//  for each loop to get every image from the gallery for 1 bag
   function showProduct(imageData) {
     console.log("hi");
     console.log(imageData);
     const myTemplate = document.querySelector(".imageTemplate").content;
     const myClone = myTemplate.cloneNode(true);
     myClone.querySelector("img").src = imageData.source_url;
-
     document.querySelector(".left").appendChild(myClone);
   }
-
-
+// new template for right side information display
+// url + id to identify specific bag
   const template = document.querySelector(".productInfoTemplate").content;
   const clone = template.cloneNode(true);
   clone.querySelector(".nameBag").textContent = item.bag_name;
@@ -47,14 +42,5 @@ function showOneItem(item) {
   clone.querySelector(".description").textContent = item.description;
   clone.querySelector(".material").textContent = item.material;
   clone.querySelector(".dimensions").textContent = item.dimensions;
- 
   document.querySelector(".right").appendChild(clone);
 }
-// showDetails();
-// function showDetails() {
-//   const template = document.querySelector("template").content;
-//   const clone = template.cloneNode(true);
-//   clone.querySelector(".price").textContent = item.price;
-
-//   document.querySelector(".products").appendChild(clone);
-// }
